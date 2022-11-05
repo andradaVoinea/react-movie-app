31e00e87e024a2066b2a67b51ae890800575bbe5
andrada <andrada.nistor@gmail.com>
2022-11-05 20:35:50 +0200

    login screen updated

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,38 @@
 import React, { useState } from "react";
 import "./LoginScreen.css";
 import SignupScreen from "./SignupScreen";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 
 function LoginScreen() {
   const [signIn, setSignIn] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleSignIn = async () => {
+    try {
+      const { email, password } = formValues;
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) navigate("/");
+  });
+
   return (
-    <div className="loginScreen">
+    <div className="loginScreen" showPassword={showPassword}>
       <div className="loginScreen_background">
         <img
           className="loginScreen_logo"
@@ -13,7 +40,7 @@ function LoginScreen() {
           alt="Netflix logo"
         />
         <button onClick={() => setSignIn(true)} className="loginScreen_button">
-          Sign in
+          Sign In
         </button>
         <div className="loginScreen_gradient"></div>
       </div>
@@ -30,14 +57,44 @@ function LoginScreen() {
             </h3>
             <div className="loginScreen_input">
               <form>
-                <input type="email" placeholder="Email Address" />
-                <button
-                  onClick={() => setSignIn(true)}
-                  className="loginScreen_getStarted"
-                >
-                  GET STARTED
-                </button>
+                <input
+                  type="email"
+                  placeholder="Email address"
+                  onChange={(e) =>
+                    setFormValues({
+                      ...formValues,
+                      [e.target.name]: e.target.value,
+                    })
+                  }
+                  name="email"
+                  value={formValues.email}
+                />
+                {showPassword && (
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) =>
+                      setFormValues({
+                        ...formValues,
+                        [e.target.name]: e.target.value,
+                      })
+                    }
+                    name="password"
+                    value={formValues.password}
+                  />
+                )}
+                {!showPassword && (
+                  <button
+                    className="loginScreen_getStarted"
+                    onClick={() => setShowPassword(true)}
+                  >
+                    Get Started
+                  </button>
+                )}
               </form>
+              <button className="loginScreen_logIn" onClick={handleSignIn}>
+                Sign Up
+              </button>
             </div>
           </>
         )}
